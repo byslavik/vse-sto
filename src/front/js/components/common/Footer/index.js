0f93500d9481f75e22ea0641ab8d447d4baadcf7
Viachaslau Vashchonak <byslavik@gmail.com>
2018-02-27 23:46:59 +0300

    copyring

--- src/front/js/components/common/Footer/index.js
+++ src/front/js/components/common/Footer/index.js
@@ -5,7 +5,8 @@ import './styles.css'
 export const Footer = () => 
   <footer className="row footer">
     <div className="col col-sm-6">
-      Все права защищены &copy; 2018
+      Все права защищены &copy; 2018<br/>
+      ИП Качалова, <a href="tel:+375291655050">+375 29 1655050</a>
     </div>
     <div className="col col-sm-6 text-right">
       <Logo />
